1d512e7194c085aa4931e83db8bd01a0ef276d6b
Diego de Miranda <diego.demiranda@icloud.com>
2024-03-06 14:08:57 -0300

    Uploading utilities
    Subindo a pasta utilities com as classes de verificação de CPF, carrinho, compras e sistema de busca.

---
+++ UdiDecola/src/utilities/Purchase.java
@@ -0,0 +1,41 @@
+package utilities;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+class Purchase {
+    private String flight;
+    private LocalDateTime dateTime;
+    private String client;
+    private double totalValue;
+    private String paymentMethod;
+    private String roomStay;
+    private LocalDateTime[] checkIn;
+    private LocalDateTime[] checkOut;
+    private double finalValue;
+
+    public Purchase(String flight, LocalDateTime dateTime, String client, double totalValue, String paymentMethod,
+                    String roomStay, LocalDateTime[] checkIn, LocalDateTime[] checkOut, double finalValue) {
+        this.flight = flight;
+        this.dateTime = dateTime;
+        this.client = client;
+        this.totalValue = totalValue;
+        this.paymentMethod = paymentMethod;
+        this.roomStay = roomStay;
+        this.checkInOutDates = checkInOutDates;
+        this.finalValue = finalValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "flight='" + flight + '\'' +
+                ", dateTime=" + dateTime +
+                ", client='" + client + '\'' +
+                ", totalValue=" + totalValue +
+                ", paymentMethod='" + paymentMethod + '\'' +
+                ", roomStay='" + roomStay + '\'' +
+                ", checkIn=" + Arrays.toString(checkIn) +
+                ", checkOut=" + Arrays.toString(checkOut) +
+                '}';
+    }
+}
+
