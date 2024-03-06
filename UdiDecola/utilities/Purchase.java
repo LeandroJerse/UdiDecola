86ba40c6b7365230f4bb8493a123a820731b7919
Diego de Miranda <diego.demiranda@icloud.com>
2024-03-06 13:56:15 -0300

    ShoppingCart e SearchSystem e Purchase update
    Correção de classes com a adição da cobrança da comissão por cada compra realizada na classe shoppinpCart e a adição de atributos pendentes na classe perchasse.

--- UdiDecola/utilities/Purchase.java
+++ UdiDecola/utilities/Purchase.java
@@ -1,19 +1,27 @@
 package utilities;
 import java.time.LocalDateTime;
-
+import java.util.Arrays;
 class Purchase {
     private String flight;
     private LocalDateTime dateTime;
     private String client;
     private double totalValue;
     private String paymentMethod;
+    private String roomStay;
+    private LocalDateTime[] checkIn;
+    private LocalDateTime[] checkOut;
+    private double finalValue;
 
-    public Purchase(String flight, LocalDateTime dateTime, String client, double totalValue, String paymentMethod) {
+    public Purchase(String flight, LocalDateTime dateTime, String client, double totalValue, String paymentMethod,
+                    String roomStay, LocalDateTime[] checkIn, LocalDateTime[] checkOut, double finalValue) {
         this.flight = flight;
         this.dateTime = dateTime;
         this.client = client;
         this.totalValue = totalValue;
         this.paymentMethod = paymentMethod;
+        this.roomStay = roomStay;
+        this.checkInOutDates = checkInOutDates;
+        this.finalValue = finalValue;
     }
 
     @Override
@@ -24,6 +32,9 @@ class Purchase {
                 ", client='" + client + '\'' +
                 ", totalValue=" + totalValue +
                 ", paymentMethod='" + paymentMethod + '\'' +
+                ", roomStay='" + roomStay + '\'' +
+                ", checkIn=" + Arrays.toString(checkIn) +
+                ", checkOut=" + Arrays.toString(checkOut) +
                 '}';
     }
 }
---
+++ UdiDecola/utilities/ShoppingCart.java
@@ -0,0 +1,32 @@
+package utilities;
+import java.util.ArrayList;
+import java.util.List;
+import utilities.Purchase;
+
+public class ShoppingCart {
+    private List<Purchase> purchases;
+    private int points;
+
+    public ShoppingCart() {
+        this.purchases = new ArrayList<>();
+        this.points = 0;
+    }
+
+    public void addAirlineTicket(String ticket, int quantity, String client, double totalValue, String paymentMethod, double commission) {
+        double finalValue = totalValue - (totalValue * commission);
+        purchases.add(new Purchase(ticket, quantity, client, finalValue, paymentMethod));
+        points += quantity;
+    }
+
+    public void addHotelRoomStay(String roomStay, int quantity, String client, double totalValue, String paymentMethod, double commission) {
+        double finalValue = totalValue - (totalValue * commission);
+        purchases.add(new Purchase(roomStay, quantity, client, finalValue, paymentMethod));
+        points += quantity;
+    }
+    public int getPoints() {
+        return points;
+    }
+
+    public
+
+}
