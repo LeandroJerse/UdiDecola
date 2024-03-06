e1d9112657641300db68947dd8243c380bc15c90
Leandro Marques <47747696+LeandroJerse@users.noreply.github.com>
2024-03-06 14:06:54 -0300

    Add files via upload

--- UdiDecola/src/accounts/ClientAccount.java
+++ UdiDecola/src/accounts/ClientAccount.java
@@ -1,5 +1,36 @@
 package accounts;
 
-public class ClientAccount {
+import java.time.LocalDate;
+
+public class ClientAccount extends TadClientAccount {
+	
+	ShoppingCart purchasesLog;
+	float VipPoint;
+	
+	
+	
+	public ClientAccount(String fullName, String cPF, String fullAdress, LocalDate birthday) {
+		super(fullName, cPF, fullAdress, birthday);
+
+	}
+	
+	public setPurchasesLog(Purchases newPurchase) {
+		purchaselog.purchases.add(newPurchase);
+	}
+	
+	public ShoppingCart getPurchasesLog() {
+		return purchasesLog;
+	}
+
+	public float getVipPoint() {
+		return VipPoint;
+	}
+	public void setVipPoint(float vipPoint) {
+		VipPoint = vipPoint;
+	}
+	
+	
+	
+	
 
 }
